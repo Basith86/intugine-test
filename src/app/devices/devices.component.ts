1efbe4cccab06b2ff77c58983dbe0f7d65c96915
Basith <basith@getmeroof.com>
2019-07-06 18:33:59 +0530

    halt

--- src/app/devices/devices.component.ts
+++ src/app/devices/devices.component.ts
@@ -11,36 +11,251 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class DevicesComponent implements OnInit {
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object,private http: HttpClient,private localStorageService: LocalStorageService) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient, private localStorageService: LocalStorageService) { }
 
 
 
-    token: any;
-    url= 'https://dl5opah3vc.execute-api.ap-south-1.amazonaws.com/latest/devices' 
-    data:any;
-    lat: number = 51.673858;
-    lng: number = 7.815982;
-    latA: 51.673858;
-    lngA: 7.815982;
+  token: any;
+  url = 'https://dl5opah3vc.execute-api.ap-south-1.amazonaws.com/latest/devices'
+  data: any;
+  lat: number = 51.673858;
+  lng: number = 7.815982;
+  latA: 51.673858;
+  lngA: 7.815982;
 
-    latB: 51.373858;
-    lngB: 7.215982;
+  latB: 51.373858;
+  lngB: 7.215982;
 
-    deviceUrl =  'https://dl5opah3vc.execute-api.ap-south-1.amazonaws.com/latest?'
-    deviceData:any;
-    dta:any;
+  deviceUrl = 'https://dl5opah3vc.execute-api.ap-south-1.amazonaws.com/latest?'
+  deviceData: any;
+  dta: any;
 
-    checked: Boolean;
-    
-    icon = { url: 'https://assetsstatic.s3.ap-south-1.amazonaws.com/navigation.svg', scaledSize: {height: 40, width: 40}
+  checked: Boolean;
+  halt: any;
+  string: any;
+  map: any;
+  len: any;
 
-    
-}
+  icon: any;
+
+  firstD:any;
+  lastD:any;
+  dist:any;
+  created:any;
+  aa:any;
+  bb:any;
+  gps:any;
+  firstLat:any;
+  firstLng:any;
+  lastLat:any;
+  lastLng:any;
 
   ngOnInit() {
 
-     this.deviceData = [{"_id":"5c617d06af92700ace9265df","input":"7878191013020b152f31c7027d27e8087ae2701414020002002551dc0d0a","tag":"GPS Info","case":"10","date":"2019-02-11T13:47:49.000Z","noSatellites":7,"gps":[23.198137777777777,79.03949333333334],"speed":20,"course":"1402","language":"English","output":"78780510002561b30D0A","imei":"0358739053342797","device":"C1","createdAt":"2019-02-11T13:47:50.141Z"},{"_id":"5c617cedaf92700ace9265d6","input":"7878191013020b152f18c7027d1e60087ae010191416000200245d9d0d0a","tag":"GPS Info","case":"10","date":"2019-02-11T13:47:24.000Z","noSatellites":7,"gps":[23.19678222222222,79.03915555555555],"speed":25,"course":"1416","language":"English","output":"787805100024703a0D0A","imei":"0358739053342797","device":"C1","createdAt":"2019-02-11T13:47:25.117Z"},{"_id":"5c617cc2af92700ace9265c9","input":"78780a13c00404000200235ef90d0a","tag":"Status Info","case":"13","terminalInfo":"c0","voltage":"Medium","gsmStrength":"Strong signal","output":"787805130023ebe10D0A","imei":"0358739053342797","device":"C1","gps":[23.19314222222222,79.0343288888889],"createdAt":"2019-02-11T13:46:42.622Z"},{"_id":"5c617c9daf92700ace9265bd","input":"7878191013020b152e04c7027d04c8087abe2029142d00020022914a0d0a","tag":"GPS Info","case":"10","date":"2019-02-11T13:46:04.000Z","noSatellites":7,"gps":[23.19314222222222,79.0343288888889],"speed":41,"course":"142d","language":"English","output":"787805100022150c0D0A","imei":"0358739053342797","device":"C1","createdAt":"2019-02-11T13:46:05.121Z"},{"_id":"5c617c89af92700ace9265af","input":"7878191013020b152d2cc7027cf844087ab4402b1418000200216fb30d0a","tag":"GPS Info","case":"10","date":"2019-02-11T13:45:44.000Z","noSatellites":7,"gps":[23.19136222222222,79.03292444444445],"speed":43,"course":"1418","language":"English","output":"78780510002127970D0A","imei":"0358739053342797","device":"C1","createdAt":"2019-02-11T13:45:45.201Z"},{"_id":"5c617c0eaf92700ace926583","input":"78780a13c00404000200206c620d0a","tag":"Status Info","case":"13","terminalInfo":"c0","voltage":"Medium","gsmStrength":"Strong signal","output":"787805130020d97a0D0A","imei":"0358739053342797","device":"C1","gps":[23.12723777777778,79.03085333333334],"createdAt":"2019-02-11T13:43:42.413Z"},{"_id":"5c617b59af92700ace926549","input":"78780a13c004030002001f95ca0d0a","tag":"Status Info","case":"13","terminalInfo":"c0","voltage":"Medium","gsmStrength":"Good signal","output":"78780513001f100e0D0A","imei":"0358739053342797","device":"C1","gps":[23.12723777777778,79.03085333333334],"createdAt":"2019-02-11T13:40:41.545Z"},{"_id":"5c617aa7af92700ace926516","input":"78780a13c004040002000ea41e0d0a","tag":"Status Info","case":"13","terminalInfo":"c0","voltage":"Medium","gsmStrength":"Strong signal","output":"78780513000e11060D0A","imei":"0358739053342797","device":"C1","gps":[23.12723777777778,79.03085333333334],"createdAt":"2019-02-11T13:37:43.049Z"},{"_id":"5c617a4caf92700ace9264ee","input":"7878191013020b15240ec4027b3564087aa5b034140100020006ee050d0a","tag":"GPS Info","case":"10","date":"2019-02-11T13:36:14.000Z","noSatellites":4,"gps":[23.12723777777778,79.03085333333334],"speed":52,"course":"1401","language":"English","output":"787805100006722a0D0A","imei":"0358739053342797","device":"C1","createdAt":"2019-02-11T13:36:12.522Z"},{"_id":"5c617a48af92700ace9264ed","input":"7878191013020b152409c4027b30bc087aa58034140300020004b3b70d0a","tag":"GPS Info","case":"10","date":"2019-02-11T13:36:09.000Z","noSatellites":4,"gps":[23.126575555555558,79.03082666666666],"speed":52,"course":"1403","language":"English","output":"78780510000451380D0A","imei":"0358739053342797","device":"C1","createdAt":"2019-02-11T13:36:08.379Z"}]
-     
+    this.deviceData = [
+      {
+        "_id": "5c754406856be00ac86ab006",
+        "input": "7878058a012b6b160d0a",
+        "date": "190226134958",
+        "case": "8A",
+        "tag": "Time Check Packet info",
+        "output": "78780B8A190226134958012b86f50D0A",
+        "imei": "0358739053368495",
+        "device": "C4",
+        "gps": [
+          13.013673333333333,
+          80.1999111111111
+        ],
+        "battery": 20,
+        "createdAt": "2019-02-26T13:49:58.350Z"
+      },
+      {
+        "_id": "5c754405856be00ac86ab000",
+        "input": "78781101035873905336849520203201012a4ae40d0a",
+        "tag": "Login",
+        "case": "01",
+        "imei": "0358739053368495",
+        "model": "2020",
+        "tzl": "3201",
+        "timezone": 8,
+        "output": "78780501012a5fd50D0A",
+        "device": "C4",
+        "createdAt": "2019-02-26T13:49:57.649Z"
+      },
+      {
+        "_id": "5c7543f9856be00ac86aafd0",
+        "input": "7878058a0127a17a0d0a",
+        "date": "190226134945",
+        "case": "8A",
+        "tag": "Time Check Packet info",
+        "output": "78780B8A190226134945012736730D0A",
+        "imei": "0358739053368495",
+        "device": "C4",
+        "gps": [
+          13.013673333333333,
+          80.1999111111111
+        ],
+        "battery": 20,
+        "createdAt": "2019-02-26T13:49:45.769Z"
+      },
+      {
+        "_id": "5c7543f9856be00ac86aafc6",
+        "input": "78781101035873905336849520203201012680880d0a",
+        "tag": "Login",
+        "case": "01",
+        "imei": "0358739053368495",
+        "model": "2020",
+        "tzl": "3201",
+        "timezone": 8,
+        "output": "78780501012695b90D0A",
+        "device": "C4",
+        "createdAt": "2019-02-26T13:49:45.025Z"
+      },
+      {
+        "_id": "5c7543f2856be00ac86aafa8",
+        "input": "78780a1380000400020125f5bd0d0a",
+        "tag": "Status Info",
+        "case": "13",
+        "terminalInfo": "80",
+        "voltage": "No power(shutdown)",
+        "battery": 20,
+        "gsmStrength": "Strong signal",
+        "output": "787805130125970f0D0A",
+        "imei": "0358739053368495",
+        "device": "C4",
+        "gps": [
+          13.013673333333333,
+          80.1999111111111
+        ],
+        "createdAt": "2019-02-26T13:49:38.368Z"
+      },
+      {
+        "_id": "5c7543f1856be00ac86aaf9c",
+        "input": "7878058a012493e10d0a",
+        "date": "190226134937",
+        "case": "8A",
+        "tag": "Time Check Packet info",
+        "output": "78780B8A190226134937012431880D0A",
+        "imei": "0358739053368495",
+        "device": "C4",
+        "gps": [
+          13.013673333333333,
+          80.1999111111111
+        ],
+        "battery": 20,
+        "createdAt": "2019-02-26T13:49:37.569Z"
+      },
+      {
+        "_id": "5c7543f0856be00ac86aaf92",
+        "input": "787811010358739053368495202032010123d7250d0a",
+        "tag": "Login",
+        "case": "01",
+        "imei": "0358739053368495",
+        "model": "2020",
+        "tzl": "3201",
+        "timezone": 8,
+        "output": "787805010123c2140D0A",
+        "device": "C4",
+        "createdAt": "2019-02-26T13:49:36.908Z"
+      },
+      {
+        "_id": "5c7543e9856be00ac86aaf87",
+        "input": "78780a138000040002012281020d0a",
+        "tag": "Status Info",
+        "case": "13",
+        "terminalInfo": "80",
+        "voltage": "No power(shutdown)",
+        "battery": 20,
+        "gsmStrength": "Strong signal",
+        "output": "787805130122e3b00D0A",
+        "imei": "0358739053368495",
+        "device": "C4",
+        "gps": [
+          13.013673333333333,
+          80.1999111111111
+        ],
+        "createdAt": "2019-02-26T13:49:29.989Z"
+      },
+      {
+        "_id": "5c7543e9856be00ac86aaf82",
+        "input": "7878058a0121c44c0d0a",
+        "date": "190226134929",
+        "case": "8A",
+        "tag": "Time Check Packet info",
+        "output": "78780B8A1902261349290121f3ab0D0A",
+        "imei": "0358739053368495",
+        "device": "C4",
+        "gps": [
+          13.013673333333333,
+          80.1999111111111
+        ],
+        "battery": 20,
+        "createdAt": "2019-02-26T13:49:29.209Z"
+      },
+      {
+        "_id": "5c7543e8856be00ac86aaf79",
+        "input": "787811010358739053368495202032010120e5be0d0a",
+        "tag": "Login",
+        "case": "01",
+        "imei": "0358739053368495",
+        "model": "2020",
+        "tzl": "3201",
+        "timezone": 8,
+        "output": "787805010120f08f0D0A",
+        "device": "C4",
+        "createdAt": "2019-02-26T13:49:28.469Z"
+      }
+    ]
+
+    this.halt = {
+      "result": [
+        "2019-02-11T13:47:50.141Z",
+        "2019-02-11T13:47:25.117Z",
+        "2019-02-11T13:46:42.622Z",
+        "2019-02-11T13:46:05.121Z",
+        "2019-02-11T13:45:45.201Z",
+        "2019-02-11T13:43:42.413Z",
+        "2019-02-11T13:40:41.545Z",
+        "2019-02-11T13:37:43.049Z",
+        "2019-02-11T13:36:12.522Z",
+        "2019-02-11T13:36:08.379Z"
+      ]
+    }
+
+    this.string = this.halt.result.map( (t) => {
+      var a= t.split('T'); 
+      var b = a[1].split('.'); 
+      return b[0];
+  })
+
+  this.firstD = this.string[0].split(':');
+  this.lastD = this.string[this.string.length-1].split(':');
+  
+  this.dist = this.calcCrow(23.198137777777777, 79.03949333333334 ,79.03949333333334, 23.126575555555558).toFixed(1);
+
+  if( ((this.firstD[0]-this.lastD[0])>0 || (this.firstD[1]-this.lastD[1]>5)) && this.dist*1000<301){
+    this.icon =  { url: 'https://assetsstatic.s3.ap-south-1.amazonaws.com/lhalt.svg', scaledSize: {height: 40, width: 40}  
+  } 
+}
+
+    // this.string = this.halt.result.map( (obj) => {
+    //   return JSON.stringify(obj)})
+
+    //   this.map = this.string.reduce(function(prev, cur) {
+    //     prev[cur] = (prev[cur] || 0) + 1;
+    //     return prev;
+    //   }, {});
+
+    //   this.len = Object.entries(this.map);
+
+    //   if(this.len[0].length<3){
+    //     this.icon =  { url: 'https://assetsstatic.s3.ap-south-1.amazonaws.com/lhalt.svg', scaledSize: {height: 40, width: 40}    
+    //   }
+    //   } else {
+    //     this.icon =  { url: 'https://assetsstatic.s3.ap-south-1.amazonaws.com/lhalt.svg', scaledSize: {height: 40, width: 40}   
+    //   }
+    // }
+
     this.checked = false;
     this.token = this.localStorageService.getLocalStorage();
     const httpOptions = {
@@ -48,22 +263,24 @@ export class DevicesComponent implements OnInit {
         'Authorization': `Bearer ${this.token.token}`
       })
     };
-    return this.http.get<any>(this.url,httpOptions).subscribe(
+    return this.http.get<any>(this.url, httpOptions).subscribe(
       res => {
-        if(res){
-         this.data = res.result;
+        if (res) {
+          this.data = res.result;
         }
       },
       err => {
 
       }
-  )    
-  
+    )
+
   }
 
- 
 
-  changed(e:any){
+
+  changed(e: any) {
+
+
     this.dta = [];
     this.deviceData = [];
     this.token = this.localStorageService.getLocalStorage();
@@ -72,29 +289,88 @@ export class DevicesComponent implements OnInit {
         'Authorization': `Bearer ${this.token.token}`
       })
     };
-   
-    console.log(e.target.value);    
-    for(var i=1; i<5; i++){
-      this.http.get<any>(this.deviceUrl+'device='+e.target.value+'&page='+i,httpOptions).subscribe(
+
+    console.log(e.target.value);
+      this.http.get<any>(this.deviceUrl + 'device=' + e.target.value + '&page=' + 1, httpOptions).subscribe(
         res => {
-          if(res){
+          if (res) {
+            this.created = res.result.map( (c) => {
+              return c.createdAt;
+          })
+
+          this.halt = {
+            "result": [
+              this.created
+            ]
+          }
+          
+          this.string = this.halt.result[0].map( (t) => {
+             this.aa = t.split('T'); 
+            this.bb = this.aa[1].split('.'); 
+            return this.bb[0];
+        })
+      
+        this.firstD = this.string[0].split(':');
+        this.lastD = this.string[this.string.length-1].split(':');
+        
+        this.gps = res.result.map( (t) => {
+          return  t.gps
+        })
+
+        this.firstLat = this.gps[0][0];
+        this.firstLng = this.gps[0][1];
+        this.lastLat = this.gps[this.gps.length-1][0];
+        this.lastLng = this.gps[this.gps.length-1][1];
+
+        this.dist = this.calcCrow(this.firstLat, this.firstLng , this.lastLat, this.lastLng).toFixed(1);
+      
+        if( ((this.firstD[0]-this.lastD[0])>0 || (this.firstD[1]-this.lastD[1]>5)) && this.dist*1000<301){
+          this.icon =  { url: 'https://assetsstatic.s3.ap-south-1.amazonaws.com/lhalt.svg', scaledSize: {height: 40, width: 40}  
+        } 
+      } else {
+        this.icon =  { url: 'https://assetsstatic.s3.ap-south-1.amazonaws.com/navigation.svg', scaledSize: {height: 40, width: 40} 
+      }
+    }
+
             this.dta.push(res.result);
-            setTimeout( () => {
+            setTimeout(() => {
               this.deviceData = this.dta.flat(1);
-            },3000)
+            }, 3000)
           }
         },
         err => {
-  
+
         }
       )
-    }
-    setTimeout( () => {
-      if(this.deviceData.length == 0){
-        alert("NO DATA AVAILABLE");
-      }
-    },5000)
+    
+
+    // setTimeout(() => {
+    //   if (this.deviceData.length == 0) {
+    //     alert("NO DATA AVAILABLE");
+    //   }
+    // }, 10000)
   }
+
+  calcCrow(lat1, lon1, lat2, lon2) 
+    {
+      var R = 6371; // km
+      var dLat = this.toRad(lat2-lat1);
+      var dLon = this.toRad(lon2-lon1);
+      var lat11 = this.toRad(lat1);
+      var lat22 = this.toRad(lat2);
+
+      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat11) * Math.cos(lat22); 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c;
+      return d;
+    }
+
+    // Converts numeric degrees to radians
+     toRad(Value) 
+    {
+        return Value * Math.PI / 180;
+    }
 
 
 
